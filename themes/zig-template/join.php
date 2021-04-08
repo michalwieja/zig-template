820a03b5cafa21bcf6f996408617106f92ef37cf
Paweł Stachel <p.stachel@qarbon.pl>
2021-04-09 01:31:43 +0200

    registration form

--- themes/zig-template/join.php
+++ themes/zig-template/join.php
@@ -1,12 +1,180 @@
-<?php
-
-echo ('test');
-
-?>
-
+<div class="join-us">
+  <div class="progress-bar">
+    <div class="progress-bar__element active">
+      <p>01<br>PRZECZYTAJ STATUS&nbsp;ZIG</p>
+    </div>
+    <div class="progress-bar__element">
+      <p>02<br>WYBIERZ WŁAŚCIWĄ SKŁADKĘ</p>
+    </div>
+    <div class="progress-bar__element">
+      <p>03<br>WYPEŁNIJ&nbsp;DEKLARACJĘ</p>
+    </div>
+    <div class="progress-bar__element">
+      <p>04<br>WYPEŁNIJ ANKIETĘ</p>
+    </div>
+    <div class="progress-bar__element">
+      <p>05<br>APLIKUJ</p>
+    </div>
+  </div>
+  <form autocomplete="off" id="join-form">
+    <div class="step regulations" id="regulations">
+      Regulamin<br/>
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+      aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+      enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+      ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+      adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+      voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+      aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
+      molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+      aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+      enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+      ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+      adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+      voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+      aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
+      molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+      aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+      enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+      ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+      adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+      voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+      aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
+      molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+      aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+      enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+      ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+      adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+      voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+      aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
+      molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+      aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+      enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+      ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+      adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+      voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+      aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
+      molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+      aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+      enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+      ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+      adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+      voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+      aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
+      molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+      aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+      enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+      ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+      adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+      voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+      aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
+      molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+    </div>
+    <div class="step">
+      <div class="contributions">
+        <div class="contributions__header">
+          <p></p>
+          <p>propozycja składek</p>
+          <p>wpisowe</p>
+          <p>składka miesięczna</p>
+        </div>
+        <div class="contributions__content">
+          <div class="contributions__row">
+              <div class="checkbox-wrapper">
+                <input type="radio" name="contribution_size" value="<49 - 540zł - 180zł"/>
+                <div class="radio-button"></div>
+              </div>
+              <p>< 49 pracowników</p>
+              <p>540 zł</p>
+              <p>180 zł</p>
+          </div>
+          <div class="contributions__row">
+            <div class="checkbox-wrapper">
+              <input type="radio" name="contribution_size" value="50-99 - 780zł - 260zł"/>
+              <div class="radio-button"></div>
+            </div>
+            <p>50-99 pracowników</p>
+            <p>780 zł</p>
+            <p>260 zł</p>
+          </div>
+          <div class="contributions__row">
+            <div class="checkbox-wrapper">
+              <input type="radio" name="contribution_size" value="100-199 - 1140zł - 380zł"/>
+              <div class="radio-button"></div>
+            </div>
+            <p>100-199 pracowników</p>
+            <p>1140 zł</p>
+            <p>380 zł</p>
+          </div>
+          <div class="contributions__row">
+            <div class="checkbox-wrapper">
+              <input type="radio" name="contribution_size" value="200-400 - 1320zł - 440zł"/>
+              <div class="radio-button"></div>
+            </div>
+            <p>200-400 pracowników</p>
+            <p>1320 zł</p>
+            <p>440 zł</p>
+          </div>
+          <div class="contributions__row">
+            <div class="checkbox-wrapper">
+              <input type="radio" name="contribution_size" value=">400 - 1800zł - 600zł"/>
+              <div class="radio-button"></div>
+            </div>
+            <p>powyżej 400 pracowników</p>
+            <p>1800 zł</p>
+            <p>600 zł</p>
+          </div>
+          <div class="contributions__row">
+            <div class="checkbox-wrapper">
+              <input type="radio" name="contribution_size" value="gminy - 1200zł - 400zł"/>
+              <div class="radio-button"></div>
+            </div>
+            <p>gminy</p>
+            <p>1200 zł</p>
+            <p>400 zł</p>
+          </div>
+          <div class="contributions__row">
+            <div class="checkbox-wrapper">
+              <input type="radio" name="contribution_size" value="gminy - 1500zł - 500zł"/>
+              <div class="radio-button"></div>
+            </div>
+            <p>banki</p>
+            <p>1500 zł</p>
+            <p>500 zł</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="step">
+      <div class="text-input__wrapper">
+        <p class="text-input__label">Imię</p>
+        <input type="text" class="text-input__input">
+      </div>
+      <div class="text-input__wrapper">
+        <p class="text-input__label">Email</p>
+        <input type="email" class="text-input__input" autocomplete="new-password">
+      </div>
+      <div class="text-input__wrapper">
+        <p class="text-input__label">Hasło</p>
+        <input type="password" class="text-input__input" autocomplete="new-password">
+      </div>
+    </div>
+  </form>
+  <div class="button-wrapper">
+    <button class="button light-blue" id="prev-button" style="opacity: 0">
+      Wróć do poprzedniego kroku
+    </button>
+    <button class="button light-blue" id="next-button">
+      Akceptuję i przechodzę dalej
+    </button>
+  </div>
+</div>
 
 <script type="text/javascript" src="/wp-content/themes/zig-template/js/join.js"></script>
-<script>
-console.warn('warn from join.php');
 
-</script>
--- themes/zig-template/js/join.js
+++ themes/zig-template/js/join.js
@@ -1,1 +1,106 @@
-console.warn('warn from join.js');
+const next = document.getElementById('next-button');
+const prev = document.getElementById('prev-button');
+const pb_el = document.querySelectorAll('.progress-bar__element');
+const steps = document.querySelectorAll('.step');
+const first_form = document.getElementById('regulations');
+const join_form = document.getElementById('join-form');
+const contributions_row = document.querySelectorAll('.contributions__row');
+const register_fields = document.querySelectorAll('.text-input__input');
+let step = 0;
+
+const user = {
+  selected_contribution: null,
+};
+
+next.addEventListener('click', () => handleFormButtonClick(1));
+prev.addEventListener('click', () => handleFormButtonClick(-1));
+contributions_row.forEach(el => {
+  el.addEventListener('click', () => handleContributionClick());
+});
+
+register_fields.forEach(input => {
+  input.addEventListener('input', (el) => handleInputInput(el));
+  input.addEventListener('focus', (el) => handleInputFocus(el, true));
+  input.addEventListener('blur', (el) => handleInputFocus(el, false));
+});
+
+const handleInputFocus = (el, focus) => {
+  if (focus) {
+    el.target.parentElement.classList.add('focus');
+  } else {
+    el.target.parentElement.classList.remove('focus');
+  }
+};
+const handleInputInput = (el) => {
+  if (el.target.value) {
+    el.target.parentElement.classList.add('has-value');
+  } else {
+    el.target.parentElement.classList.remove('has-value');
+  }
+};
+
+const handleContributionClick = () => {
+  const radios = document.querySelectorAll('input[name=contribution_size]');
+  radios.forEach(el => {
+    const sibling = el.nextElementSibling;
+    if (el.checked) {
+      sibling.classList.add('active');
+    } else {
+      sibling.classList.remove('active');
+    }
+  });
+};
+
+const handleFormButtonClick = (action) => {
+  setActiveStep(action);
+  generateButtons();
+};
+
+const generateButtons = () => {
+  switch (step) {
+    case 0: {
+      prev.style.opacity = 0;
+      return;
+    }
+    case 2: {
+      next.innerText = 'Wyślij deklarację';
+      return;
+    }
+    default: {
+      prev.style.opacity = 1;
+      next.style.opacity = 1;
+      next.innerText = 'Akceptuję i przechodzę dalej';
+      return;
+    }
+  }
+};
+
+const setActiveStep = (action) => {
+  if (step + action < 0 || step + action > 4) return;
+  step += action;
+  if (step < 0 || step > 4) return;
+  pb_el.forEach((el, index) => {
+    if (index < step) {
+      el.classList.remove('active');
+      el.classList.add('past');
+      return;
+    }
+    if (index === step) {
+      el.classList.remove('past');
+      el.classList.add('active');
+      return;
+    }
+    if (index > step) {
+      el.classList.remove('active');
+    }
+  });
+  steps.forEach((el, index) => {
+    if (index === step) {
+      el.style.maxHeight = 'unset';
+    } else {
+      el.style.maxHeight = 0;
+    }
+  });
+  join_form.scrollTop = 0;
+  first_form.style.marginLeft = `${-(step * 1000)}px`;
+};
