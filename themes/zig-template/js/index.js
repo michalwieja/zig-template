1af24ecc20c2875a380a6ee1976cb2f95c8df23b
Paweł Stachel <p.stachel@qarbon.pl>
2021-04-16 00:57:38 +0200

    final registration form

--- themes/zig-template/js/index.js
+++ themes/zig-template/js/index.js
@@ -2,8 +2,15 @@
 
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.header__nav');
+const header = document.querySelector('.header');
 
 hamburger.addEventListener('click', () => handleHamburgerClick());
+
+if (window.location.pathname === '/dolacz/') {
+  header.classList.add('inverted');
+} else {
+  header.classList.remove('inverted');
+}
 
 const handleHamburgerClick = () => {
   hamburger.classList.toggle('active');
@@ -27,9 +34,9 @@ setInterval(() => {
 //carousel arrows
 const removeDefaultArrows = () => {
   const rightArrows = document.querySelectorAll('.owl-nav .owl-next span');
-  rightArrows.forEach(arrow => arrow.remove())
+  rightArrows.forEach(arrow => arrow.remove());
   const leftArrows = document.querySelectorAll('.owl-nav .owl-prev span');
-  leftArrows.forEach(arrow => arrow.remove())
+  leftArrows.forEach(arrow => arrow.remove());
 };
 
 document.addEventListener("DOMContentLoaded", function () {
--- themes/zig-template/js/join.js
+++ themes/zig-template/js/join.js
@@ -1,5 +1,3 @@
-// import { validateMail, validateNonExistent } from './validator';
-
 const next = document.getElementById('next-button');
 const prev = document.getElementById('prev-button');
 const pb_el = document.querySelectorAll('.progress-bar__element');
@@ -9,12 +7,20 @@ const join_form = document.getElementById('join-form');
 const contributions_row = document.querySelectorAll('.contributions__row');
 const register_fields = document.querySelectorAll('.text-input__input');
 const close_modal_button = document.getElementById('close-modal-button');
+const error_list = document.querySelectorAll('.um-field-error');
+const post_message = document.querySelector('.um-postmessage');
+const form_error = document.getElementById('form-error');
+
+const form_id = 291;
 let step = 0;
+let request_errors = [];
 
 const body_factory = {
-  selected_contribution: null,
-  email: null,
-  password: null,
+  contribution_proposal: null,
+  entry_fee: null,
+  monthly_fee: null,
+  username: null,
+  user_password: null,
   name: null,
 };
 
@@ -28,7 +34,7 @@ const error = {
 
 close_modal_button.addEventListener('click', () => {
   document.querySelector('html').classList.remove('modal-open');
-  document.getElementById('um-submit-btn').click();
+  window.location.href = '/'; // TODO
 });
 next.addEventListener('click', () => handleFormButtonClick(1));
 prev.addEventListener('click', () => handleFormButtonClick(-1));
@@ -43,7 +49,7 @@ register_fields.forEach(input => {
 });
 
 const validateStepRegulations = () => {
-  if (!user.selected_contribution) {
+  if (!(user.contribution_proposal && user.entry_fee && user.monthly_fee)) {
     error.selected_contribution = 'Proszę wybrać wysokość składki';
     contributions_row.forEach(el => el.classList.add('error'));
     next.classList.add('disabled');
@@ -55,19 +61,21 @@ const validateStepRegulations = () => {
   return error.selected_contribution;
 };
 
-const validateStepDeclaration = (field = null, value = null) => {
-  if (field === 'email') {
-    error.email = !validateNonExistent(value || user.email)
+const validateStepDeclaration = (field = null, value = null, custom_error = null) => {
+  if (custom_error) {
+    error[field] = custom_error;
+  } else if (field === 'username') {
+    error.username = !validateNonExistent(value || user.username)
       ? 'Pole nie może być puste'
-      : !validateMail(value || user.email) ? 'Email nie jest poprawny' : null;
+      : !validateMail(value || user.username) ? 'Email nie jest poprawny' : null;
   } else if (field) {
     error[field] = !validateNonExistent(value || user[field]) ? 'Pole nie może być puste' : null;
   } else {
-    error.email = !validateNonExistent(user.email)
+    error.username = !validateNonExistent(user.username)
       ? 'Pole nie może być puste'
-      : !validateMail(user.email) ? 'Email nie jest poprawny' : null;
+      : !validateMail(user.username) ? 'Email nie jest poprawny' : null;
     error.name = !validateNonExistent(user.name) ? 'Pole nie może być puste' : null;
-    error.password = !validateNonExistent(user.password) ? 'Pole nie może być puste' : null;
+    error.user_password = !validateNonExistent(user.user_password) ? 'Pole nie może być puste' : null;
   }
 
   Object.entries(error).forEach(([k, v]) => {
@@ -92,6 +100,7 @@ const handleInputFocus = (el, focus) => {
     el.target.parentElement.classList.remove('focus');
   }
 };
+
 const handleInputInput = (el) => {
   validateStepDeclaration(el.target.name, el.target.value);
   const res = !Object.values(error).every(el => !el);
@@ -113,7 +122,9 @@ const handleContributionClick = () => {
   radios.forEach(el => {
     const sibling = el.nextElementSibling;
     if (el.checked) {
-      user.selected_contribution = el.value;
+      user.contribution_proposal = el.value.split('|')[0];
+      user.entry_fee = el.value.split('|')[1];
+      user.monthly_fee = el.value.split('|')[2];
       sibling.classList.add('active');
     } else {
       sibling.classList.remove('active');
@@ -123,6 +134,10 @@ const handleContributionClick = () => {
 };
 
 const handleFormButtonClick = (action) => {
+  if (action < 0) {
+    if (step + action < 0) return;
+    setActiveStep(action);
+  }
   let has_errors = false;
   if (step === 1) {
     has_errors = validateStepRegulations();
@@ -158,25 +173,85 @@ const generateButtons = () => {
   }
 };
 
+const generateError = (msg) => {
+  if (msg.includes('username is already taken') || msg.includes('email is already linked to an existing account')) {
+    return 'Użytkownik z takim adresem email już istnieje';
+  }
+  if (msg.includes('required')) {
+    return 'Pole jest wymagane';
+  }
+  return 'Wartość nie jest poprawna';
+};
+
 const fillJoinForm = () => {
-  const name = document.getElementById('name-254');
-  const email = document.getElementById('user_email-254');
-  const password = document.getElementById('user_password-254');
-  const selected_contribution = document.getElementById('membership-level-254');
+  const name = document.getElementById(`name-${form_id}`);
+  const username = document.getElementById(`username-${form_id}`);
+  const user_password = document.getElementById(`user_password-${form_id}`);
+  const contribution_proposal = document.getElementById(`contributions_proposal-${form_id}`);
+  const entry_fee = document.getElementById(`entry_fee-${form_id}`);
+  const monthly_fee = document.getElementById(`monthly_fee-${form_id}`);
 
   name.value = user.name;
-  email.value = user.email;
-  password.value = user.password;
-  selected_contribution.value = user.selected_contribution;
+  username.value = user.username;
+  user_password.value = user.user_password;
+  contribution_proposal.value = user.contribution_proposal;
+  entry_fee.value = user.entry_fee;
+  monthly_fee.value = user.monthly_fee;
+
+  document.getElementById('um-submit-btn').click();
+};
+
+const fillInput = (val, el) => {
+  if (val) {
+    el.value = val;
+    el.parentElement.classList.add('has-value');
+  } else {
+    el.parentElement.classList.remove('has-value');
+  }
+};
+
+const getDataFromForm = () => {
+  const name = document.getElementById(`name-${form_id}`);
+  const name_input = document.getElementById('input-name-input');
+  const username = document.getElementById(`username-${form_id}`);
+  const username_input = document.getElementById('input-username-input');
+
+  user.name = name.value;
+  fillInput(name.value, name_input);
+  user.username = username.value;
+  fillInput(username.value, username_input);
+};
+
+const checkForErrors = () => {
+  if (error_list && error_list.length) {
+    form_error.innerText = 'Wystąpił błąd - uzupełnij formularz ponownie';
+    form_error.classList.add('visible');
+    request_errors = [...error_list].map((err) => ({
+      field: err.parentElement.id.replace(`um_field_${form_id}_`, ''),
+      message: generateError(err.innerText)}));
+    getDataFromForm();
+    setActiveStep(1);
+    request_errors.forEach((err) => {
+      validateStepDeclaration(err.field, null, err.message);
+    });
+    return;
+  } else if (post_message) {
+    document.querySelector('html').classList.add('modal-open');
+  } else {
+    request_errors = [];
+  }
+  form_error.innerText = '';
+  form_error.classList.remove('visible');
 };
 
 const setActiveStep = (action) => {
-  if (step + action < 0 || step + action > 4) return;
+  if (step + action > 4) return;
   step += action;
   if (step < 0 || step > 4) return;
+  next.classList.remove('disabled');
   if (step === 3) {
     fillJoinForm();
-    document.querySelector('html').classList.add('modal-open')
+    next.classList.add('disabled');
   }
   pb_el.forEach((el, index) => {
     if (index < step) {
@@ -203,3 +278,5 @@ const setActiveStep = (action) => {
   join_form.scrollTop = 0;
   first_form.style.marginLeft = `${-(step * 1000)}px`;
 };
+
+checkForErrors();
